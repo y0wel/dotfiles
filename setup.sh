80ba27238171c2e89ddd327db27679d07dff4628
Tobias <matz_tobias@web.de>
2025-01-09 16:42:09 +0100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "Initiate Homebrew setup..."
+# Check if Homebrew is installed
+if command -v brew &> /dev/null
+then
+  echo "Homebrew is already installed."
+else
+  echo "Homebrew is not installed. Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Make sure Homebrew is up-to-date
+echo "Updating Homebrew..."
+brew update
+
+# Install packages from the Brewfile
+if [ -f ~/dev/dotfiles/Brewfile ]; then
+  echo "Installing packages from Brewfile..."
+  brew bundle --file=~/dev/dotfiles/Brewfile
+else
+  echo "No Brewfile found at ~/dev/dotfiles/Brewfile."
+fi
+
+# Cleanup Homebrew
+echo "Running Homebrew cleanup..."
+brew cleanup
+
+echo "Homebrew setup is done!"
